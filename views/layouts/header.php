107b9fb17634b4f7f4814be8ef8bfae15db0087b
Arga Abiyyu <argaabiyyu4@gmail.com>
2026-09-07 11:18:01 +0700

    auto: 2026-09-07 11:18:01

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -7,7 +7,7 @@
     <link rel="icon" type="image/svg+xml" href="/geprek-geh/public/favicon.svg">
     <link rel="stylesheet" href="/geprek-geh/public/fonts/fonts.css">
     <link rel="stylesheet" href="/geprek-geh/vendor/css/lenis.css">
-    <link rel="stylesheet" href="/geprek-geh/public/css/style.css?v=20260906">
+    <link rel="stylesheet" href="/geprek-geh/public/css/style.css?v=20260907">
 </head>
 <body>
 
