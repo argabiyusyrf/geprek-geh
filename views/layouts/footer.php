9d1870bf987a6da20e07aa29752d3a61b534c664
Arga Abiyyu <argaabiyyu4@gmail.com>
2026-09-09 00:40:01 +0700

    auto: 2026-09-09 00:40:01

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -39,6 +39,6 @@
 </div>
 
 <script src="/geprek-geh/vendor/lenis/lenis.min.js"></script>
-<script src="/geprek-geh/public/js/app.js?v=20260909a"></script>
+<script src="/geprek-geh/public/js/app.js?v=20260909b"></script>
 </body>
 </html>
