6dd7503c99269d5a7bd23c078b27b41f193c9d54
Arga Abiyyu <argaabiyyu4@gmail.com>
2026-09-11 16:20:01 +0700

    auto: 2026-09-11 16:20:01

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -28,7 +28,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="/geprek-geh/public/fonts/fonts.css">
     <link rel="stylesheet" href="/geprek-geh/vendor/css/lenis.css">
-    <link rel="stylesheet" href="/geprek-geh/public/css/style.css?v=20260911c">
+    <link rel="stylesheet" href="/geprek-geh/public/css/style.css?v=20260911d">
 
     <?= SeoController::organizationJsonLd() ?>
     <?= $page_jsonld ?? '' ?>
