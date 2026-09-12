ff39c907df4aea7bd43b9dc3a8955c6faaea57c2
Arga Abiyyu <argaabiyyu4@gmail.com>
2026-09-12 15:53:01 +0700

    auto: 2026-09-12 15:53:01

--- views/layouts/admin-header.php
+++ views/layouts/admin-header.php
@@ -6,7 +6,7 @@
     <title>Admin — Geprek Geh</title>
     <link rel="stylesheet" href="/geprek-geh/public/fonts/fonts.css">
     <link rel="stylesheet" href="/geprek-geh/vendor/css/lenis.css">
-    <link rel="stylesheet" href="/geprek-geh/public/css/style.css?v=20260912m">
+    <link rel="stylesheet" href="/geprek-geh/public/css/style.css?v=20260912n">
 </head>
 <body class="admin-body">
 
